7aacc369cbb02e1a3a3eb6889495e1018d8b8a2e
CIBILLON Benoit <benoit.cibillon@gmail.com>
2026-05-23 21:09:37 +0200

    feat(init): add tag + run inherit from parent

--- src/converter/node.ts
+++ src/converter/node.ts
@@ -2,8 +2,10 @@ import type { IRunOptions, XmlComponent } from "docx";
 import type { Node } from "dom-parser";
 import type { ConverterContext } from "../context/convertercontext";
 
+export type GetChildrenFct = (node: Node, run: IRunOptions, context: ConverterContext) => XmlComponent[];
+
 export abstract class NodeConverter {
   
-  abstract convert(node: Node, run: IRunOptions, children: XmlComponent[], context: ConverterContext): XmlComponent | XmlComponent[];
+  abstract convert(node: Node, run: IRunOptions, context: ConverterContext, children: GetChildrenFct): XmlComponent | XmlComponent[];
 
 }
